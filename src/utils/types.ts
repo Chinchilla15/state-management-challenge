90c39c0eccc21535f0aaefae2b6ab33b10fc9603
Alessandro Chinchilla <ale030401@gmail.com>
2024-12-20 13:53:05 -0600

    feat: types for components

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,6 +2,18 @@ export interface Character {
   id: string;
   name: string;
   image: string;
+  species: string;
+  status: string;
+  gender: string;
+  origin: {
+    name: string;
+  };
+  location: {
+    name: string;
+  };
+  episode: {
+    name: string;
+  }[];
 }
 
 export interface FavoritesState {
@@ -14,4 +26,46 @@ export const initialState: FavoritesState = {
 
 export interface PageLayoutProps {
   children: React.ReactNode;
+}
+
+export interface NavItemProps {
+  name: string;
+  species: string;
+  id: string;
+  onSelect: (id: string) => void;
+  isSelected?: boolean;
+}
+
+type InfoTitle =
+  | "Name"
+  | "Species"
+  | "Status"
+  | "Gender"
+  | "Location"
+  | "Origin"
+  | "Episode"
+  | `Episode ${number}`;
+
+type InfoItemType = {
+  Name: { value: string };
+  Species: { value: string };
+  Status: { value: string };
+  Gender: { value: string };
+  Location: { value: string };
+  Origin: { value: string };
+  Episode: { value: string };
+  [key: `Episode ${number}`]: { value: string };
+};
+
+export type CharacterInfoItemProps = {
+  [K in InfoTitle]: {
+    title: K;
+    value: InfoItemType[K]["value"];
+  };
+}[InfoTitle];
+
+export interface CharacterInfoProps {
+  children?: React.ReactNode;
+  imageSource?: string;
+  imageAlt?: string;
 }
